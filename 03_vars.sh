fbe9b912b9bb0b4070b73d30f365874cf34689be
Anto <japanto20@gmail.com>
2023-09-19 17:23:44 -0500

    Create servers

--- 03_vars.sh
+++ 03_vars.sh
@@ -1,2 +1,7 @@
 DATE=2023-09-19
 echo "Hello, Good Morning, Today date is ${DATE}"
+
+#command substitution
+DATE=${date}
+
+echo "Hello, Good Morning, Today date is dynamically displaying as  ${DATE}"
