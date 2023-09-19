7c43c5a105d45c5325cd6c16fa4204ca459b64ab
Anto <japanto20@gmail.com>
2023-09-19 17:39:06 -0500

    Create servers

--- 03_vars.sh
+++ 03_vars.sh
@@ -10,4 +10,4 @@ echo "Hello, Good Morning, Today date is dynamically displaying as ${DATE}"
 
 # shellcheck disable=SC1116
 ADD=$((2+3))
-echo "Addition of2+3 = ${ADD}"
+echo "Addition of 2+3 = ${ADD}"
