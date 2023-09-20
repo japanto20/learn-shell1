4f36b9912aef41bd30b481df0fb42da996e5d458
Anto <japanto20@gmail.com>
2023-09-20 12:56:12 -0500

    Create servers

--- 05_functions.sh
+++ 05_functions.sh
@@ -1,6 +1,12 @@
 # Declare a function
 xyz() {
   echo Hello from function
+  echo first argument - $1
+  echo second argument - $2
+  echo all argument - $*
+  echo no of argument - $#
 }
+# Main Program
 #call a function
-xyz
+xyz 123 456
+
