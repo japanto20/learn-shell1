c832bb0c2f17147e93ebcf3dc7ff0746b770f173
Anto <japanto20@gmail.com>
2023-09-20 20:25:47 -0500

    Create servers

--- 06_conditions.sh
+++ 06_conditions.sh
@@ -3,8 +3,12 @@ string=$2
 
 if [ "${number}" -eq 5 ]; then
   echo number is 5
+else
+  echo number is not 5
 fi
 
 if [ "${string}" == abc ]; then
   echo string is abc
+else
+  echo string is not abc
 fi
