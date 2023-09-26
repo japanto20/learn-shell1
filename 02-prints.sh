41a61046ab30ec392643e0abf06d6290331f606b
Anto <japanto20@gmail.com>
2023-09-25 20:23:16 -0500

    Create servers

--- 02-prints.sh
+++ 02-prints.sh
@@ -12,9 +12,14 @@ echo Hello World today
  #Cyan    36
 
  # \e[0m  > zero code is going to reset the color, meaning if we enable color we need to disable it other wise
- color will continue
+ #color will continue
 
  echo -e "\e[31mHello in Red Color\e[0m"
  echo -e "\e[32mHello in Green Color\e[0m"
  echo -e "\e[33mHello in yellow Color\e[0m"
  echo -e "\e[34mHello in Blue Color\e[0m"
+
+
+# sometimes we need to print mutiple lines
+
+echo -e "line1\nline2\nline3"
